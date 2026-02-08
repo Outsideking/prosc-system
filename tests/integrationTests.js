a5d0639885b03f92942b894e2bbfe2cc6c734042
Rufio Dinoto(Musk) <rufiodinoto244@gmail.com>
2026-02-08 22:33:55 +0700

    Add files via upload

--- tests/integrationTests.js
+++ tests/integrationTests.js
@@ -1,1 +1,16 @@
+const axios = require('axios');
+const base64 = Buffer.from('admin:supersecret').toString('base64');
 
+async function testDynamicAPI() {
+    const response = await axios.post('http://localhost:3000/prosc/registerSchema', {
+        route: "testAPI",
+        processLogic: async (input) => ({ message: `Hello ${input.name}` })
+    }, { headers: { owner: "admin" } });
+    console.log(response.data);
+
+    const apiResponse = await axios.post('http://localhost:3000/dynamic/testAPI/v1', { name: "Rufio" },
+        { headers: { authorization: base64 } });
+    console.log(apiResponse.data);
+}
+
+testDynamicAPI();
